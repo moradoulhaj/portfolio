9ccc8710f8bd3f79911aea09b505e13c84561141
moradoulhaj <moradoulhaj.off@gmail.com>
2024-07-16 21:52:20 +0100

    Services section done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import "./App.css";
 import { useEffect } from "react";
 import Aos from "aos";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function App() {
-  useEffect(() => { 
+  useEffect(() => {
     Aos.init();
-  },[])
+  }, []);
   return (
     <div className="container">
       <Header />
+      <main>
+        <Hero />
+        <Services />
+      </main>
+
       <Footer />
     </div>
   );
